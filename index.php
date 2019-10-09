d37e29b0a77686abbff54b67a3370f1e5e5b38fe
masoudi <rmrasoulmasoudi@gmail.com>
2019-10-09 15:38:06 +0330

    user management

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require __DIR__ . '/vendor/autoload.php';
 
 use Twig\Environment;
@@ -9,63 +12,182 @@ DEFINE('HOST', '127.0.0.1');
 DEFINE('USER', 'root');
 DEFINE('PASSWORD', 'a');
 DEFINE('DB_NAME', 'dling');
+DEFINE('APP_NAME', 'جیلینگ');
+DEFINE('APP_SITE', 'gling.ir');
+session_start();
+
 
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
-
-$path = ltrim($_SERVER['REQUEST_URI'], '/');
-$elements = explode('/', $path);
-array_shift($elements);
-
-if (count($elements) == 1) {
-    $path = urldecode($elements[0]);
-    if ($path == "") {
-        gotoMainPage($twig);
-    } else if ($path == "ثبت_نام") {
-        echo $twig->render('register.twig');
-    } else if ($path == "register") {
-        register();
-    } else {
-        $conn = getConnection();
-        $stmt = $conn->prepare("SELECT * FROM product WHERE url=?");
-        $stmt->bind_param("s", $path);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        #echo $twig->render('product.twig',["id"=>$row['id'],"title"=>$row['title'] );
-        $stmt->close();
-        $conn->close();
-        showProduct(urldecode($path), $conn);
-    }
-} else {
-    header('HTTP/1.1 404 Not Found');
-    echo "not found";
-}
-
-function register($twig) {
-    $name = $_POST["name"];
-    $mail = $_POST["mail"];
-    $password = $_POST["password"];
-    $mobile = $_POST["mobile"];
-
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'logger' => [
+            'name' => 'slim-app',
+            'level' => Monolog\Logger::DEBUG,
+            'path' => __DIR__ . '/../logs/app.log',
+        ],
+    ],
+];
+$app = new \Slim\App($config);
+$app->get('/', function (Request $request, Response $response, $args) use ($twig, $app) {
     $conn = getConnection();
-    $stmt = $conn->prepare("SELECT * FROM user WHERE mail=? OR mobile=?");
-    $stmt->bind_param("ss", $mail, $mobile);
+    $stmt = $conn->prepare("SELECT * FROM product");
     $stmt->execute();
     $result = $stmt->get_result();
-    if (mysqli_num_rows($result) == 0) {
-        $conn->prepare("INSERT INTO user(name,mail,mobile,password)VALUES(?,?,?,?)");
-        $stmt->bind_param("ssss", $name, $mail, $mobile, $password);
+    $list = array();
+    while ($row = $result->fetch_assoc()) {
+        array_push($list, [ "category" => $row["category"], "url" => $row["url"], "title" => $row["title"]]);
+    }
+    $stmt->close();
+    $conn->close();
+    $response->getBody()->write($twig->render('main.twig', ["products" => $list, "user" => getCurrentUser()]));
+})->setName('main');
+
+$app->get('/register', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $response->getBody()->write($twig->render('register.twig', []));
+    }
+})->setName('register');
+$app->get('/login', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $response->getBody()->write($twig->render('login.twig', []));
+    }
+})->setName('login');
+$app->get('/logout', function (Request $request, Response $response, $args) use ($twig, $app) {
+    session_unset();
+    session_destroy();
+    return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+})->setName('logout');
+
+$app->post('/dologin', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $mail = $_POST["mail"];
+        $password = $_POST["password"];
+
+        $conn = getConnection();
+        $stmt = $conn->prepare("SELECT * FROM user WHERE mail=? AND password=?");
+        $stmt->bind_param("ss", $mail, $password);
         $stmt->execute();
         $result = $stmt->get_result();
-        if ($result) {
-            gotoMainPage($twig);
+        if (mysqli_num_rows($result) != 0) {
+            $row = $result->fetch_assoc();
+            setCurrentUser($row["name"], $row["id"], $row["mail"], $row["mobile"]);
+            return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
         } else {
-            
+            $response->getBody()->write($twig->render('login.twig', ["error" => "رایانامه یا رمز اشتباه است"]));
         }
-    } else {
-        echo json_encode($result);
     }
+})->setName('dologin');
+
+$app->post('/save_user', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $name = $_POST["name"];
+        $mail = $_POST["mail"];
+        $password = $_POST["password"];
+        $mobile = $_POST["mobile"];
+
+        $conn = getConnection();
+        $stmt = $conn->prepare("SELECT * FROM user WHERE mail=? OR mobile=?");
+        $stmt->bind_param("ss", $mail, $mobile);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if (mysqli_num_rows($result) == 0) {
+            $stmt = $conn->prepare("INSERT INTO user(name,mail,mobile,password)VALUES(?,?,?,?)");
+            $stmt->bind_param("ssss", $name, $mail, $mobile, $password);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $response->getBody()->write($twig->render('register.twig', ["success" => true]));
+        } else {
+            $response->getBody()->write($twig->render('register.twig', ["error" => "شماره موبایل یا رایانامه قبلا ثبت شده است"]));
+        }
+    }
+})->setName('save_user');
+
+$app->get('/profile', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (!isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $response->getBody()->write($twig->render('profile.twig', ["user" => getCurrentUser()]));
+    }
+})->setName('profile');
+
+$app->post('/update_user', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (!isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $mobile = $_POST["mobile"];
+        $mail = $_POST["mail"];
+        $id = getCurrentUser()["id"];
+
+        $conn = getConnection();
+        $stmt = $conn->prepare("UPDATE user SET name=?,password=?,mobile=?,mail=? WHERE id=?");
+        $stmt->bind_param("sssss", $name, $password, $mobile, $mail, $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        setCurrentUser($name, $id, $mail, $mobile);
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    }
+})->setName('update_user');
+
+
+$app->get('/forget', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $response->getBody()->write($twig->render('forget.twig', []));
+    }
+})->setName('forget');
+
+$app->post('/sendpass', function (Request $request, Response $response, $args) use ($twig, $app) {
+    if (isLogged()) {
+        return $response->withRedirect($app->getContainer()->get('router')->pathFor("main"));
+    } else {
+        $mail = $_POST["mail"];
+        $conn = getConnection();
+        $stmt = $conn->prepare("SELECT * FROM user WHERE mail=?");
+        $stmt->bind_param("s", $mail);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if (mysqli_num_rows($result) == 0) {
+            $response->getBody()->write($twig->render('forget.twig', ["error" => "چنین ایمیلی در سیستم ثبت نشده است"]));
+        } else {
+            $row = $result->fetch_assoc();
+            $message = "رمز عبور شما: " + $row["password"];
+            sendMail($mail, $message, "بازیابی رمز", $twig);
+            $response->getBody()->write($twig->render('forget.twig', ["success" => true]));
+        }
+    }
+})->setName('profile');
+
+$app->get('/{name}', function(Request $request, Response $response, $args) use ($twig, $app) {
+    $response->getBody()->write($args["name"]);
+});
+$app->run();
+
+function isLogged() {
+    return getCurrentUser() != null;
+}
+
+function setCurrentUser($name, $id, $mail, $mobile) {
+    $_SESSION["user"] = ["name" => $name, "id" => $id, "mail" => $mail, "mobile" => $mobile];
+}
+
+function getCurrentUser() {
+    if (isset($_SESSION["user"])) {
+        return $_SESSION["user"];
+    }
+    return null;
 }
 
 function getConnection() {
@@ -77,16 +199,10 @@ function getConnection() {
     return $conn;
 }
 
-function gotoMainPage($twig) {
-    $conn = getConnection();
-    $stmt = $conn->prepare("SELECT * FROM product");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $list = array();
-    while ($row = $result->fetch_assoc()) {
-        array_push($list, [ "category" => $row["category"], "url" => $row["url"], "title" => $row["title"]]);
-    }
-    $stmt->close();
-    $conn->close();
-    echo $twig->render('main.twig', ["products" => $list]);
+function sendMail($to, $message, $title, $twig) {
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: <info@' + APP_SITE + '>' . "\r\n";
+    $body = $twig->render('mail.twig', ["title" => $title, "message" => $message, "app_site" => APP_SITE, "app_name" => APP_NAME]);
+    mail($to, $title, $body, $headers);
 }
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,7 +6,24 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Zend\\Diactoros\\' => array($vendorDir . '/zendframework/zend-diactoros/src'),
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Slim\\Views\\' => array($vendorDir . '/slim/twig-view/src'),
+    'Slim\\Psr7\\' => array($vendorDir . '/slim/psr7/src'),
+    'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
+    'Psr\\Http\\Server\\' => array($vendorDir . '/psr/http-server-handler/src', $vendorDir . '/psr/http-server-middleware/src'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
+    'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
+    'Nyholm\\Psr7\\' => array($vendorDir . '/nyholm/psr7/src'),
+    'Nyholm\\Psr7Server\\' => array($vendorDir . '/nyholm/psr7-server/src'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
+    'Http\\Message\\' => array($vendorDir . '/php-http/message-factory/src'),
+    'Http\\Factory\\Guzzle\\' => array($vendorDir . '/http-interop/http-factory-guzzle/src'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'Fig\\Http\\Message\\' => array($vendorDir . '/fig/http-message-util/src'),
+    'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
 );
